3a8065cebae3104e53bf77e13802645b3b56cbdc
Taras Lyashenko <t.d.liashenko@gmail.com>
2023-07-20 18:01:50 +0600

    Push new project

---
+++ src/main/java/com/example/TeacherBot/TeacherBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.TeacherBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeacherBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeacherBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TeacherBot/TeacherBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TeacherBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeacherBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
